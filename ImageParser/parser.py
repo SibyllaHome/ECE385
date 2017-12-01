cb81737d8d03fcdf406f05c055b48869cc691c26
Jordan <jordanw2@illinois.edu>
2017-12-01 10:46:40 -0600

    updated image parser python program

--- ImageParser/parser.py
+++ ImageParser/parser.py
@@ -1,74 +1,45 @@
 from PIL import Image
 
-im = Image.open('../assets/background.jpg')
-f = open('../assets/memory_content.txt','w')
+def read_rgb_8_bit(image_name, output_im, output_file, output_hex):
+    image = Image.open(image_name)
+    (w , h) = image.size
+    image_rgb = image.convert('RGB')
+
+    im_out = Image.new('RGB', (w, h))
+    pixels_out = im_out.load()
+
+    for y in range(0,h):
+        for x in range(0,w):
+            (r, g, b) = image_rgb.getpixel((x, y))
+            # b_r, b_g, b_b = bin(r).lstrip('-0b').zfill(8), bin(g).lstrip('-0b').zfill(8), bin(b).lstrip('-0b').zfill(8)
+            # print (b_r, b_g, b_b)
+            # print ""
+            r = r & int('11100000',2)
+            g = (g & int('11100000',2))
+            b = (b & int('11000000',2))
+
+            pixels_out[x,y] = (r,g,b)
+
+            # perform bitshift
+            r = r
+            g = g >> 3
+            b = b >> 6
+            o = r + g + b
+
+            h_string = hex(o).lstrip('-0x').zfill(2)
+            output_hex.write(h_string)
+            if (x % 2 == 0):
+                output_file.write("mem_array[" + str((y*w+x)/2) + "] = 16'h" + h_string)
+            if (x % 2 == 1):
+                output_file.write(h_string + ";\n")
+
+    im_out.save(output_im)
+
+text = open('../assets/memory_content.txt','w')
 hexfile = open('../assets/content.hex','w')
 
-# get size
-(w , h) = im.size
-print(w,h)
+read_rgb_8_bit('../assets/640x480/background.jpg','../assets/640x480/converted/background.jpg', text, hexfile)
+read_rgb_8_bit('../assets/640x480/fighter1.png','../assets/640x480/converted/fighter1.png', text, hexfile)
 
-im_out = Image.new('RGB', (w, h))
-pixels_out = im_out.load()
-
-# convert to rgb
-rgb_im = im.convert('RGB')
-
-for y in range(0,h):
-    for x in range(0,w):
-        (r, g, b) = rgb_im.getpixel((x, y))
-        # b_r, b_g, b_b = bin(r).lstrip('-0b').zfill(8), bin(g).lstrip('-0b').zfill(8), bin(b).lstrip('-0b').zfill(8)
-        # print (b_r, b_g, b_b)
-        # print ""
-        r = r & int('11100000',2)
-        g = (g & int('11100000',2))
-        b = (b & int('11000000',2))
-
-        pixels_out[x,y] = (r,g,b)
-
-        # perform bitshift
-        r = r
-        g = g >> 3
-        b = b >> 6
-        o = r + g + b
-
-        h_string = hex(o).lstrip('-0x').zfill(2)
-        hexfile.write(h_string)
-        if (x % 2 == 0):
-            f.write("mem_array[" + str((y*w+x)/2) + "] = 16'h" + h_string)
-        if (x % 2 == 1):
-            f.write(h_string + ";\n")
-
-im_out.save("../assets/converted.jpg")
-
-im_p1 = Image.open('../assets/spritesheet_p1.png')
-im_p1 = im_p1.convert('RGB')
-
-for y in range(0,h):
-    for x in range(0,w):
-        (r, g, b) = im_p1.getpixel((x, y))
-        # b_r, b_g, b_b = bin(r).lstrip('-0b').zfill(8), bin(g).lstrip('-0b').zfill(8), bin(b).lstrip('-0b').zfill(8)
-        # print (b_r, b_g, b_b)
-        # print ""
-        r = r & int('11100000',2)
-        g = (g & int('11100000',2))
-        b = (b & int('11000000',2))
-
-        pixels_out[x,y] = (r,g,b)
-
-        # perform bitshift
-        r = r
-        g = g >> 3
-        b = b >> 6
-        o = r + g + b
-
-        h_string = hex(o).lstrip('-0x').zfill(2)
-        hexfile.write(h_string)
-        if (x % 2 == 0):
-            f.write("mem_array[" + str(((y*w+x)/2)+38400) + "] = 16'h" + h_string)
-        if (x % 2 == 1):
-            f.write(h_string + ";\n")
-            
-im_out.save("../assets/converted_sprite.jpg")
-f.close()
+text.close()
 hexfile.close()
